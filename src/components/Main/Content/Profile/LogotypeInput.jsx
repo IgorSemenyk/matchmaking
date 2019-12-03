fc183a02884d814d48058915d3195ddbf7eecbe9
Anton <tony.knytkt@gmail.com>
2019-12-04 01:26:37 +0200

    modification upload input form

--- src/components/Main/Content/Profile/LogotypeInput.jsx
+++ src/components/Main/Content/Profile/LogotypeInput.jsx
@@ -1,29 +1,63 @@
 import React from 'react';
+import {Field, reduxForm} from "redux-form";
+import {connect} from "react-redux";
+import * as axios from "axios";
+
+let mapStateToProps = (state) => {
+    return {
+
+    }
+};
+
+const adaptFileEventToValue = delegate =>
+    e => delegate(e.target.files[0]);
+
+const FileInputHTML = ({
+                       input: {
+                           value: omitValue,
+                           onChange,
+                           onBlur,
+                           ...inputProps
+                       },
+                       meta: omitMeta,
+                       ...props
+                   }) =>
+    <input
+        onChange={adaptFileEventToValue(onChange)}
+        onBlur={adaptFileEventToValue(onBlur)}
+        type="file"
+        {...inputProps}
+        {...props}
+    />;
+
+let FileInputForm = (props) => {
+    return(
+        <form onSubmit={props.handleSubmit} >
+            <label>
+                Set new logotype:
+                <Field component={FileInputHTML} name={'upload'} />
+                <button>Set</button>
+            </label>
+        </form>
+    )
+};
 
 class FileInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.fileInput = React.createRef();
-    }
-    handleSubmit(event) {
-        event.preventDefault();
-        alert(
-            `Selected file - ${
-                this.fileInput.current.files[0].name
-            }`
+    onSubmit = (formData) => {
+        console.log(formData);
+        axios.post('https://anigo.com.ua/api/files', formData, {
+            headers: { 'Content-Type': 'multipart/form-data' }
+        }).then(
+            res => console.log(res)
         );
-    }
+    };
     render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Set new logotype:
-                    <input type="file" ref={this.fileInput} onChange={this.handleSubmit} />
-                </label>
-            </form>
-        );
+        return <FileInputRedux {...this.props} onSubmit={this.onSubmit} />
     }
 }
 
-export default FileInput;
+
+const FileInputRedux = reduxForm({form: 'logotype'})(FileInputForm);
+
+export default connect(mapStateToProps)(FileInput);
+
